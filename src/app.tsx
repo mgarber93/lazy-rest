6ab1a2d99ba08d13f16622a24d2469826fad8d63
mgarber93 <matthewgarber@proton.me>
2024-02-09 21:26:13 -0500

    Form

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,30 @@
 import {createRoot} from 'react-dom/client';
+import React, {FormEvent} from 'react';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<h2>Hello from React!</h2>);
+const App = () => {
+  const [inputValue, setValue] = React.useState('');
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    alert(`User input: ${inputValue}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        User Prompt:
+        <input type="text" value={inputValue} onChange={handleChange}/>
+      </label>
+      <input type="submit" value="Submit"/>
+    </form>
+  );
+};
+
+root.render(<App/>);
