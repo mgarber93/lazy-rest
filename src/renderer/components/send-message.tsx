9e7ceedc2d1452118127469aa38d0b25a36c3b53
mgarber93 <matthewgarber@proton.me>
2024-02-20 00:41:00 -0500

    Calculate rows

--- src/renderer/components/send-message.tsx
+++ src/renderer/components/send-message.tsx
@@ -9,27 +9,14 @@ const TextArea = styled.textarea`
     width: 100%;
     border: 1px solid var(--background-color-0);
     margin-top: auto;
-    background-color: var(--background-color-1);
+    background-color: var(--background-color-0);
     color: var(--text-color);
     position: sticky;
     bottom: 1rem;
-    height: 2.5rem;
     border-radius: 0.5rem;
-    padding: 0.3rem 0.5rem 1rem 0.5rem;
+    padding: 0.3rem 0.5rem 0.3rem 0.5rem;
     font-size: larger;
 `
-
-function FileUpload() {
-  return <div>
-    <input type="file" id="file-upload"/>
-    <label htmlFor="file-upload">
-      <svg width="24" height="24" viewBox="0 0 24 24">
-        <path fill="currentColor" className="attachFile"
-              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/>
-      </svg>
-    </label>
-  </div>;
-}
 
 export function SendMessage() {
   const [inputValue, setValue] = React.useState('');
@@ -41,16 +28,17 @@ export function SendMessage() {
   
   const handleKeyPress: React.KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
     if (e.key === 'Enter' && !e.shiftKey && inputValue) {
+      e.preventDefault();
       const prompt = createContent(inputValue, 'this', false);
       dispatch(respond(prompt))
       dispatch(generateResponse(prompt));
       setValue('');
     }
   };
-  
+  const rows = Math.max(inputValue.split('\n').length, 1);
   return (
     <TextArea
-      rows={1}
+      rows={rows}
       placeholder="Message agent"
       onChange={handleChange}
       onKeyPressCapture={handleKeyPress}
