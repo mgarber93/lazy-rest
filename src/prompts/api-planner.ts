f9aaa14581c127b8ab7f75e6664ce274f36ce6e3
mgarber93 <matthewgarber@proton.me>
2024-03-10 03:18:33 -0400

    Format

--- src/prompts/api-planner.ts
+++ src/prompts/api-planner.ts
@@ -4,12 +4,12 @@ export type TApi = 'tmdb' | 'spotify';
 
 const apiToIclExamples = {
   "tmdb": `Example 1:
-  User query: give me some movies performed by Tony Leung.
-  Plan step 1: search person with name "Tony Leung"
+User query: give me some movies performed by Tony Leung.
+Plan step 1: search person with name "Tony Leung"
 API response: Tony Leung's person_id is 1337
 Plan step 2: collect the list of movies performed by Tony Leung whose person_id is 1337
 API response: Shang-Chi and the Legend of the Ten Rings, In the Mood for Love, Hero
-  Thought: I am finished executing a plan and have the information the user asked for or the data the used asked to create
+Thought: I am finished executing a plan and have the information the user asked for or the data the used asked to create
 Final Answer: Tony Leung has performed in Shang-Chi and the Legend of the Ten Rings, In the Mood for Love, Hero
   
   Example 2:
@@ -25,11 +25,11 @@ Final Answer: Laeta Kalogridis wrote the screenplay for the most famous movie di
 `,
   "spotify": `Example 1:
 User query: set the volume to 20 and skip to the next track.
-  Plan step 1: set the volume to 20
+Plan step 1: set the volume to 20
 API response: Successfully called PUT /me/player/volume to set the volume to 20.
 Plan step 2: skip to the next track
 API response: Successfully called POST /me/player/next to skip to the next track.
-  Thought: I am finished executing a plan and completed the user's instructions
+Thought: I am finished executing a plan and completed the user's instructions
 Final Answer: I have set the volume to 20 and skipped to the next track.
   
   Example 2:
@@ -40,11 +40,11 @@ Plan step 2: Continue. search for the most popular songs by Coldplay (4gzpq5DPGx
 API response: Successfully called GET /artists/4gzpq5DPGxSnKTe4SA8HAU/top-tracks to get the most popular songs by Coldplay. The most popular songs by Coldplay are Yellow (3AJwUDP919kvQ9QcozQPxg), Viva La Vida (1mea3bSkSGXuIRvnydlB5b).
 Plan step 3: make a playlist called "Love Coldplay"
 API response: Successfully called GET /me to get the user id. The user id is xxxxxxxxx.
-  Plan step 4: Continue. make a playlist called "Love Coldplay"
+Plan step 4: Continue. make a playlist called "Love Coldplay"
 API response: Successfully called POST /users/xxxxxxxxx/playlists to make a playlist called "Love Coldplay". The playlist id is 7LjHVU3t3fcxj5aiPFEW4T.
-  Plan step 5: Add the most popular songs by Coldplay, Yellow (3AJwUDP919kvQ9QcozQPxg), Viva La Vida (1mea3bSkSGXuIRvnydlB5b), to playlist "Love Coldplay" (7LjHVU3t3fcxj5aiPFEW4T)
+Plan step 5: Add the most popular songs by Coldplay, Yellow (3AJwUDP919kvQ9QcozQPxg), Viva La Vida (1mea3bSkSGXuIRvnydlB5b), to playlist "Love Coldplay" (7LjHVU3t3fcxj5aiPFEW4T)
 API response: Successfully called POST /playlists/7LjHVU3t3fcxj5aiPFEW4T/tracks to add Yellow (3AJwUDP919kvQ9QcozQPxg), Viva La Vida (1mea3bSkSGXuIRvnydlB5b) in playlist "Love Coldplay" (7LjHVU3t3fcxj5aiPFEW4T). The playlist id is 7LjHVU3t3fcxj5aiPFEW4T.
-  Thought: I am finished executing a plan and have the data the used asked to create
+Thought: I am finished executing a plan and have the data the used asked to create
 Final Answer: I have made a new playlist called "Love Coldplay" containing Yellow and Viva La Vida by Coldplay.
 `,
 }
