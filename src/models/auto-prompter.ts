44c96e2fc2e25c2f19b2db7a0976d7e7d8a8d4dd
mgarber93 <matthewgarber@proton.me>
2024-03-13 01:58:20 -0400

    Add auto prompter to convo
    Conversation has a autoprompter

---
+++ src/models/auto-prompter.ts
@@ -0,0 +1,1 @@
+export type TAutoPrompter = 'rest api'
--- src/models/conversation.ts
+++ src/models/conversation.ts
@@ -1,8 +1,11 @@
 import {AuthoredContent} from './content';
+import {TAutoPrompter} from './auto-prompter';
+
 
 export interface Conversation {
   id: string;
   content: AuthoredContent[];
   title: string;
   responder?: string;
+  autoPrompter?: TAutoPrompter
 }
