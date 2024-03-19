589ef8a1ac99613570297353f9d7c4d12f440613
mgarber93 <matthewgarber@proton.me>
2024-03-19 10:40:40 -0400

    Add jest

---
+++ src/models/content.test.ts
@@ -0,0 +1,6 @@
+import {createContent} from './content';
+
+test('create content', () => {
+  const content = createContent('my message', '123', 'me', 'system')
+  expect(content.message).toBe('my message');
+})
