1dd03932b1e0bc66f880a6425cf0bcd456bf4396
mgarber93 <mgarber93@gmail.com>
2024-03-29 11:00:13 -0400

    Add test coverage

--- src/renderer/utils/sort-date.test.ts
+++ src/renderer/utils/sort-date.test.ts
@@ -8,12 +8,12 @@ describe('sort date', () => {
       'Sun Mar 24 2024 00:12:26 GMT-0400 (Eastern Daylight Time)',
       'Sun Mar 24 2024 00:17:46 GMT-0400 (Eastern Daylight Time)'
     ];
-    const sorted = dates.sort(newestToOldest)
+    const sorted = dates.sort(newestToOldest);
     expect(sorted).toStrictEqual([
       'Sun Mar 24 2024 00:17:46 GMT-0400 (Eastern Daylight Time)',
       'Sun Mar 24 2024 00:12:26 GMT-0400 (Eastern Daylight Time)',
       'Sun Mar 24 2024 00:07:23 GMT-0400 (Eastern Daylight Time)',
       'Sat Mar 23 2024 23:46:43 GMT-0400 (Eastern Daylight Time)',
-    ])
+    ]);
   });
 });
