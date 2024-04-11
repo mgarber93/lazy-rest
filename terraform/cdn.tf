889420c35d032e255715d2416b6ca5895d8ee3e8
mgarber93 <matthewgarber@proton.me>
2024-04-11 03:09:33 -0400

    Add bucket
    Deploys but cloudfront isn't routing traffic correctly

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -2,10 +2,59 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+locals {
+  site = "lazy-rest.com"
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name               = local.site
+  subject_alternative_names = ["*.${local.site}"]
+  validation_method         = "DNS"
+
+  tags = {
+    Name = "lazy-rest-ssl-certificate"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  // │ Elements of a set are identified only by their value and don't have any separate index or key to select with, so it's only possible to perform operations across all elements of the set.
+  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
+  zone_id = "Z05410901PMU87KKB8A64"
+  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+module "bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "lazy-rest"
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::lazy-rest/*"
+      }
+    ]
+  }
+  POLICY
+}
+
 module "cdn" {
   source = "terraform-aws-modules/cloudfront/aws"
 
-  aliases = ["mattgarber.dev"]
+  #   aliases = [""]
 
   comment = "CloudFront"
   enabled             = true
@@ -21,7 +70,7 @@ module "cdn" {
 
   origin = {
     webfront = {
-      domain_name = "mattgarber.dev"
+      domain_name = module.bucket.s3_bucket_bucket_domain_name
       custom_origin_config = {
         http_port              = 80
         https_port             = 443
@@ -42,7 +91,9 @@ module "cdn" {
   }
 
   viewer_certificate = {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
+    acm_certificate_arn = aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
   }
+
+  depends_on = [aws_acm_certificate_validation.cert]
 }
