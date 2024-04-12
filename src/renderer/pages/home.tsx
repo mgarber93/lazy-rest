1aa668bde1925aeab8e72e067f3cbf721944788e
mgarber93 <matthewgarber@proton.me>
2024-04-11 20:35:32 -0400

    Add api section

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -15,19 +15,57 @@ const Section = styled.section`
   .card {
     max-width: 600px;
   }
+
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &.blur {
+    filter: blur(1px);
+  }
+
+  .reset {
+    filter: none !important;
+  }
+
+  *.background {
+    position: absolute;
+  }
+
+  &.secondary {
+    background-color: slategray;
+  }
 `
+
+function ApiCard() {
+  return <span>hello world</span>;
+}
 
 export function Home() {
   return (
     <PageContainer>
-      <Section>
-        <h4>Providers</h4>
-        <div className="providers">
-          <Card>
-            <OpenAiConfigForm/>
-          </Card>
-        </div>
-      </Section>
+      <div className={"h-100 d-flex flex-md-column justify-content-around p-5"}>
+        <Section>
+          <h4>Providers</h4>
+          <div className="providers">
+            <Card>
+              <OpenAiConfigForm/>
+            </Card>
+          </div>
+        </Section>
+        <Section className="p-5 secondary">
+          <div className="background"></div>
+          <div className="reset">
+            <h4>Api</h4>
+            <div className="providers">
+              <Card>
+                hello world
+              </Card>
+            </div>
+          </div>
+        </Section>
+      </div>
     </PageContainer>
   );
 }
