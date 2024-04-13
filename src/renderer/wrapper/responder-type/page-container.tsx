9c20dad7c69eb05df7edc166d4fccad4812440e4
mgarber93 <matthewgarber@proton.me>
2024-04-13 11:54:39 -0400

    Better background

--- src/renderer/wrapper/responder-type/page-container.tsx
+++ src/renderer/wrapper/responder-type/page-container.tsx
@@ -8,6 +8,7 @@ const Container = styled.div`
   width: 100%;
   max-height: 100vh;
   overflow-y: hidden;
+  background: linear-gradient(var(--background-color-0), var(--background-color-1) 45%);
 `
 
 export function PageContainer(props: { children: ReactNode }) {
