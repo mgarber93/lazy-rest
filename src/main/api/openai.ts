ae2b94306f2c9d0ca4d8885ebb4acac13d24f68e
mgarber93 <mgarber93@gmail.com>
2024-04-13 16:14:19 -0400

    Refactor OpenAI provider and add tests
    Refactored the OpenAI provider into a class and added related unit tests. This provides cleaner organization and better test coverage. Additionally, logic for validating tool calls in content model was adjusted for improved robustness.

--- src/main/api/openai.ts
+++ src/main/api/openai.ts
@@ -1,19 +1,82 @@
-import OpenAI from 'openai';
+import OpenAI, {ClientOptions} from 'openai';
 import {ChatCompletionMessageParam} from 'openai/resources';
 import {AuthoredContent, ContentDelta, isToolCall} from '../../models/content';
 import windowSender from '../window-sender';
 
 
-const openai = new OpenAI({});
-
-// https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
-export async function getModels(): Promise<string> {
-  return ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo-preview'].join(',');
-  // const models = await openai.models.list();
-  // return models.data
-  //   .filter(item => item.object === 'model' && item.id.startsWith('gpt'))
-  //   .map(item => item.id)
-  //   .join(',');
+export class OpenAiProvider {
+  // https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
+  private openai: OpenAI;
+  constructor(apiKey: string, baseUrl?: string) {
+    const config = {apiKey} as ClientOptions;
+    if (baseUrl) {
+      config.baseURL = baseUrl
+    }
+    this.openai = new OpenAI(config);
+  }
+  async getModels(): Promise<string> {
+    return ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo-preview'].join(',');
+  }
+  async testConnection(): Promise<string> {
+    const models = await this.openai.models.list();
+    return models.data
+      .filter(item => item.object === 'model' && item.id.startsWith('gpt'))
+      .map(item => item.id)
+      .join(',');
+  }
+  async prompt(model: string, content: AuthoredContent[]): Promise<{
+    role: "system" | "assistant" | "user",
+    content: string
+  }> {
+    const messages = content
+      .map(mapAuthoredContentToChatCompletion)
+    const chatCompletion = await this.openai.chat.completions.create({
+      model,
+      messages,
+    });
+    return chatCompletion.choices[0].message;
+  }
+  async agentWithHttp(model: string, messages: ChatCompletionMessageParam[]) {
+    const result = await this.openai.chat.completions.create({
+      messages,
+      model,
+      tools: [
+        {
+          type: 'function',
+          function: {
+            name: "GET",
+            description: "Retrieves data from the server without modifying it.",
+            parameters: {
+              type: "object",
+              properties: {
+                endpoint: {
+                  type: 'string',
+                  description: 'endpoint to call (e.g. /search?customer=123)',
+                },
+              },
+              required: ["endpoint"],
+            },
+          },
+        },
+      ],
+    });
+    return result.choices[0].message;
+  }
+  async streamedPrompt(model: string, content: AuthoredContent[], chatId: string, messageId: string): Promise<void> {
+    const messages = content
+      .map(mapAuthoredContentToChatCompletion)
+    const stream = await this.openai.chat.completions.create({
+      model,
+      messages: messages,
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      const delta = chunk.choices[0]?.delta?.content || "";
+      process.stdout.write(delta);
+      windowSender.send('message-delta', {delta, chatId, messageId} as ContentDelta)
+    }
+    windowSender.send('message-delta', {delta: '', chatId, messageId, closed: true});
+  }
 }
 
 export function mapAuthoredContentToChatCompletion(content: AuthoredContent): ChatCompletionMessageParam {
@@ -26,7 +89,7 @@ export function mapAuthoredContentToChatCompletion(content: AuthoredContent): Ch
     case "tool": {
       if (!isToolCall(content))
         throw new Error(`content isn't a tool call`);
-      
+
       return {
         role: content.role,
         content: content.message,
@@ -37,75 +100,4 @@ export function mapAuthoredContentToChatCompletion(content: AuthoredContent): Ch
 }
 
 
-export async function prompt(model: string, content: AuthoredContent[]): Promise<{
-  role: "system" | "assistant" | "user",
-  content: string
-}> {
-  const messages = content
-    .map(mapAuthoredContentToChatCompletion)
-  const chatCompletion = await openai.chat.completions.create({
-    model,
-    messages,
-  });
-  return chatCompletion.choices[0].message;
-}
 
-export async function streamedPrompt(model: string, content: AuthoredContent[], chatId: string, messageId: string): Promise<void> {
-  const messages = content
-    .map(mapAuthoredContentToChatCompletion)
-  const stream = await openai.chat.completions.create({
-    model,
-    messages: messages,
-    stream: true,
-  });
-  for await (const chunk of stream) {
-    const delta = chunk.choices[0]?.delta?.content || "";
-    process.stdout.write(delta);
-    windowSender.send('message-delta', {delta, chatId, messageId} as ContentDelta)
-  }
-  windowSender.send('message-delta', {delta: '', chatId, messageId, closed: true});
-}
-
-interface ToolParameter {
-  type: 'object' | 'string' | 'number';
-  enum: string[];
-  description: string;
-}
-
-interface ObjectParameter extends ToolParameter {
-  type: 'object';
-  required: string[];
-}
-
-export interface Tool {
-  name: string;
-  description: string;
-  parameters: Record<string, ToolParameter>;
-}
-
-export async function agentWithHttp(model: string, messages: ChatCompletionMessageParam[]) {
-  const result = await openai.chat.completions.create({
-    messages,
-    model,
-    tools: [
-      {
-        type: 'function',
-        function: {
-          name: "GET",
-          description: "Retrieves data from the server without modifying it.",
-          parameters: {
-            type: "object",
-            properties: {
-              endpoint: {
-                type: 'string',
-                description: 'endpoint to call (e.g. /search?customer=123)',
-              },
-            },
-            required: ["endpoint"],
-          },
-        },
-      },
-    ],
-  });
-  return result.choices[0].message;
-}
