9e81c47a9f9a7a721ae26774cb4a4f066e54effd
mgarber93 <matthewgarber@proton.me>
2024-04-16 19:34:20 -0400

    Implement provider manager
    This class manages provider config and highlevel llm queries like get fast, get cheap, etc

---
+++ src/main/provider-manager.ts
@@ -0,0 +1,22 @@
+import {OpenAiConfiguration} from '../models/provider-config';
+import OpenAI from 'openai';
+
+// manages provider config and high level llm queries like get fast, get cheap, etc
+export class ProviderManager {
+  openAiConfig: OpenAiConfiguration | null = null;
+  
+  getOpenAi() {
+    if (!this.openAiConfig) {
+      throw new Error('Not configured for openai!');
+    }
+    const {baseUrl, apiKey} = this.openAiConfig;
+    return new OpenAI({baseURL: baseUrl, apiKey});
+  }
+  
+  setOpenAiConfig(openAiConfig: OpenAiConfiguration): void {
+    this.openAiConfig = openAiConfig;
+  }
+}
+
+
+export default new ProviderManager();
