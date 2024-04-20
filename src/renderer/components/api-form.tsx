e13fbc1eb15feb96f8f4a5ffba91ea4a710897d8
mgarber93 <mgarber93@gmail.com>
2024-04-20 03:07:14 -0400

    Linting

--- src/renderer/components/api-form.tsx
+++ src/renderer/components/api-form.tsx
@@ -12,23 +12,33 @@ export function ApiForm() {
     <h5>{name}</h5>
     <Label>Name</Label>
     <Control type="text" placeholder="Api name (eg spotify)" value={name}
-             onChange={(event: { target: { value: SetStateAction<string>; }; }) => setName(event.target.value)}/>
+             onChange={(event: {
+               target: { value: SetStateAction<string>; };
+             }) => setName(event.target.value)}/>
 
     <Label>Base URL</Label>
     <Control type="text" placeholder="Base URL" value={baseUrl}
-             onChange={(event: { target: { value: SetStateAction<string>; }; }) => setBaseUrl(event.target.value)}/>
+             onChange={(event: {
+               target: { value: SetStateAction<string>; };
+             }) => setBaseUrl(event.target.value)}/>
 
     <Label>Client ID</Label>
     <Control type="text" placeholder="Client ID" value={clientId}
-                  onChange={(event: { target: { value: SetStateAction<string>; }; }) => setClientId(event.target.value)}/>
+             onChange={(event: {
+               target: { value: SetStateAction<string>; };
+             }) => setClientId(event.target.value)}/>
 
     <Label>Client Secret</Label>
     <Control type="text" placeholder="Client secret" value={clientSecret}
-                  onChange={(event: { target: { value: SetStateAction<string>; }; }) => setClientSecret(event.target.value)}/>
+             onChange={(event: {
+               target: { value: SetStateAction<string>; };
+             }) => setClientSecret(event.target.value)}/>
 
     <Label>Api swagger</Label>
     <Control type="file" placeholder="Swagger OAS file"
-                  onChange={(event: { target: { value: SetStateAction<string>; }; }) => setClientSecret(event.target.value)}/>
+             onChange={(event: {
+               target: { value: SetStateAction<string>; };
+             }) => setClientSecret(event.target.value)}/>
 
     <Footer>
       <Button variant="primary" type="submit" size={"sm"}>Submit</Button>
