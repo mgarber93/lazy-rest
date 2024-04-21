cc9da981d883aeae093962e94c4f59d7d038ca7d
mgarber93 <matthewgarber@proton.me>
2024-04-21 19:57:18 -0400

    Restyle button

--- src/renderer/styled/button.ts
+++ src/renderer/styled/button.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  background: linear-gradient(45deg, var(--blush), var(--coral));
-  border: none;
+  background: var(--call-to-action);
+  border: 1px solid var(--primary-0);
   height: 2.5rem;
   border-radius: var(--border-radius);
   padding: 0.5rem 1rem;
+
+  &:hover {
+    box-shadow: 4px 0px 2rem var(--primary-2);
+  }
 `
