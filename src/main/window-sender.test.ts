4dbffffc01b57fd7a41124dde07c30399d7f9214
mgarber93 <mgarber93@gmail.com>
2024-04-22 21:18:31 -0400

    Implement promise map

---
+++ src/main/window-sender.test.ts
@@ -0,0 +1,43 @@
+import {WindowSender} from './window-sender';
+import {v4} from 'uuid';
+
+describe('WindowSender', () => {
+  it('should resolve with the sent values before hasFinishedLoading has been called', async () => {
+    const windowSender = new WindowSender();
+    const argOne = 'load-oas';
+    const argTwo = v4();
+    const argThree = {arg: 'hello world'}
+    let callback = (...args: any[]) => {
+      expect(args[0]).toEqual(argOne);
+      expect(args[1]).toEqual(argTwo);
+      expect(args[2]).toEqual(argThree);
+    };
+    windowSender.send(argOne, argTwo, argThree)
+      .then(callback);
+    expect(windowSender['promiseMap'].size).toBe(1);
+    windowSender.hasFinishedLoading(callback);
+    // callback shouldn't have been called yet because there are no messages in queue
+    // some async process goes on then invokes the callback
+    // windowSender.callback(id, arg);
+    // promise resolves
+  });
+  it('should resolve with the sent values after have finished loading has happened', async () => {
+    const windowSender = new WindowSender();
+    const argOne = 'load-oas';
+    const argTwo = v4();
+    const argThree = {arg: 'hello world'}
+    let callback = (...args: any[]) => {
+      expect(args[0]).toEqual(argOne);
+      expect(args[1]).toEqual(argTwo);
+      expect(args[2]).toEqual(argThree);
+    };
+    windowSender.hasFinishedLoading(callback);
+    windowSender.send(argOne, argTwo, argThree)
+      .then(callback);
+    expect(windowSender['promiseMap'].size).toBe(1);
+    // callback shouldn't have been called yet because there are no messages in queue
+    // some async process goes on then invokes the callback
+    // windowSender.callback(id, arg);
+    // promise resolves
+  });
+});
--- src/main/window-sender.ts
+++ src/main/window-sender.ts
@@ -1,9 +1,11 @@
-export type TChannel = "message-delta" | "tool-request" | "tool-approval";
+export type TChannel = "message-delta" | "tool-request" | "tool-approval" | "load-oas";
 
+// first id is id for co-ordination
 export type TSender = (eventName: string, ...args: any[]) => void;
 
-export class WindowSender {
+export class WindowSender<T = any> {
   private _sender: TSender | null = null;
+  private promiseMap = new Map<string, (value: unknown) => void>();
   
   constructor(private _queue: { eventName: TChannel, args: any[] }[] = []) {
   }
@@ -15,11 +17,25 @@ export class WindowSender {
     }
   }
   
-  send(eventName: TChannel, ...args: any[]) {
-    if (this._sender) {
-      this._sender(eventName, ...args);
-    } else {
-      this._queue.push({eventName, args});
+  send(eventName: TChannel, id: string, ...args: any[]): Promise<T> {
+    if (this.promiseMap.has(id)) {
+      // throw duplication error
+      throw new Error(`${id} is already registered`)
+    }
+
+    return new Promise((resolve, reject) => {
+      this.promiseMap.set(id, resolve);
+      if (this._sender) {
+        this._sender(eventName, id, ...args);
+      } else {
+        this._queue.push({eventName, args: [id, ...args]});
+      }
+    })
+  }
+  callback(id:string , response: T) {
+    const resolve = this.promiseMap.get(id);
+    if (resolve) {
+      return resolve(response);
     }
   }
 }
