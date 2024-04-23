82a64ea7ee9a9309aaf49d32eaa03c798420058c
mgarber93 <mgarber93@gmail.com>
2024-04-22 23:42:00 -0400

    Api loop load-oas

--- src/main/api-loop.ts
+++ src/main/api-loop.ts
@@ -1,4 +1,3 @@
-import {loadOasSpec} from './oas-loader';
 import {Conversation} from '../models/conversation';
 import {oasToDescriptions, treeShake} from './oas-filter';
 import {parseCalls} from './utils';
@@ -11,26 +10,46 @@ import {chat} from './api/api';
 import {agentWithHttp} from './api/openai';
 import {getModel} from '../models/responder';
 import ChatCompletionMessage = OpenAI.ChatCompletionMessage;
+import {OpenApiSpec} from '../models/open-api-spec';
+import {v4} from 'uuid';
+import tree from '../renderer/components/tree';
+
+async function loadOas(): Promise<OpenApiSpec[]> {
+  const id = v4();
+  // how does the main process invoke the renderer?
+  const response = await windowSender.asyncSend("load-oas", id);
+  return response;
+}
 
 
 export type TAgent = "planner" | "selector" | "executor";
 
-export async function apiAgentLoop(user: Conversation): Promise<{ content: string, role: string }> {
+/**
+ * Flagship organization for product
+ * @param user
+ */
+export async function restApiOrganization(user: Conversation): Promise<{ content: string, role: string }> {
   const planner = startAgentConversation(user, "planner");
   const plan = await chat(planner.responder, planner.content);
   console.log(`p1:`, plan.content);
-  const oasSpec = await loadOasSpec('spotify');
-  const filtered = oasToDescriptions(oasSpec)
-  
+  const oasSpec = await loadOas();
+  const filtered = oasSpec.reduce((acc: string, spec) => acc + oasToDescriptions(spec), '');
+
   const selector = startAgentConversation(user, 'selector', JSON.stringify(filtered, null, 2), plan.content);
   const selectedPlan = await chat(selector.responder, selector.content);
   console.log(`a1:`, selectedPlan.content);
-  
+
   const calls = parseCalls(selectedPlan.content)
   let toolPlan: ChatCompletionMessage;
-  
+
   for (const plannedCall of calls) {
-    const specForPlannedCall = treeShake(oasSpec, calls);
+    const specForPlannedCall = oasSpec.reduce((acc: Record<string, any>, spec: OpenApiSpec) => {
+      const treeShook = treeShake(spec, calls)
+      for (const key in acc) {
+        acc[key] = treeShook[key]
+      }
+      return acc;
+    }, {} as Record<string, any>);
     const executor = startAgentConversation(user, 'executor', JSON.stringify(specForPlannedCall, null, 2));
     // @todo parsing plan
     const messages: ChatCompletionMessageParam[] = executor.content
