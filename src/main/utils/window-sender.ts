b84fd61767a35670005503150bf1d599243b4ec7
mgarber93 <mgarber93@gmail.com>
2024-04-26 19:11:34 -0400

    Refactor generic

--- src/main/utils/window-sender.ts
+++ src/main/utils/window-sender.ts
@@ -2,10 +2,9 @@ import {v4} from 'uuid';
 
 export type TChannel = "message-delta" | "load-oas" | 'callback' | 'approval';
 
-// first id is id for co-ordination
 export type TSender = (eventName: string, ...args: any[]) => void;
 
-export class WindowSender<T = any> {
+export class WindowSender {
   private _sender: TSender | null = null;
   private promiseMap = new Map<string, (value: unknown) => void>();
   constructor(private _queue: { eventName: TChannel, args: any[] }[] = []) {
@@ -16,9 +15,6 @@ export class WindowSender<T = any> {
       this._sender(message.eventName, ...message.args);
     }
   }
-  /**
-   * Internal detail to window sender that it uses a queue
-   */
   private sendOrQueue(eventName: TChannel, args: any[]) {
     if (this._sender) {
       this._sender(eventName, ...args);
@@ -29,25 +25,30 @@ export class WindowSender<T = any> {
   send(eventName: TChannel, ...args: any[]): void {
     return this.sendOrQueue(eventName, args);
   }
-  asyncSend(eventName: TChannel, ...args: any[]): Promise<T> {
+
+  /**
+   * Async send allows the renderer process (received by sender fxn) to complete some async process and then return
+   * value of type T using the callback method defined on this class.
+   * @param eventName
+   * @param args
+   */
+  asyncSend<T = any>(eventName: TChannel, ...args: any[]): Promise<T> {
     const id = v4()
     return new Promise((resolve, reject) => {
       if (this.promiseMap.has(id)) {
-        // throw duplication error
-        throw new Error(`${id} is already registered`)
+        throw new Error(`${id} is already registered (do we need to set a random seed?)`);
       }
       this.promiseMap.set(id, resolve);
 
       const nextArgs = [id, ...args];
       if (this._sender) {
-        // we're resolving right away
          this._sender(eventName, ...nextArgs);
       } else {
         this._queue.push({eventName, args: nextArgs});
       }
     });
   }
-  callback(id:string, response: T) {
+  callback(id:string, response: any) {
     const resolve = this.promiseMap.get(id);
     if (resolve) {
       this.promiseMap.delete(id);
