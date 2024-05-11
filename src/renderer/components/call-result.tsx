f0d359c8467ecee9b3cde81190a4e8ed04fb1810
mgarber93 <matthewgarber@proton.me>
2024-05-11 18:02:58 -0400

    Styling tweaks

--- src/renderer/components/call-result.tsx
+++ src/renderer/components/call-result.tsx
@@ -3,10 +3,17 @@ import remarkGfm from 'remark-gfm'
 import styled from 'styled-components'
 
 const Div = styled.div`
+  background-color: var(--background-color-2);
+  border-radius: var(--border-radius-comfy);
+  padding: 1rem;
 `
 
 export function ResultOfCall({result}: { result: object }) {
-  const serialized = JSON.stringify(result, null, 2)
+  const serialized = JSON.stringify(result, (key: string, value: any) => {
+    if (Array.isArray(value) && value.length > 3)
+      return `[${value.slice(0, 3).join(', ')}, ...]`
+    return value
+  }, 2)
   
   return <Div>
     <Markdown className="content" remarkPlugins={[remarkGfm]}>{
