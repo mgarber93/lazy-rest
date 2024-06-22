f8ae0f65dc876c16c02045a79c94b0157e197938
mgarber93 <mgarber93@gmail.com>
2024-06-22 08:06:21 -0400

    Remove snapshots

--- src/main/utils/oas-filter.test.ts
+++ src/main/utils/oas-filter.test.ts
@@ -5,24 +5,8 @@ import {OpenAPI} from 'openapi-types'
 
 const openApiSpec = spec as unknown as OpenAPI.Document
 
-test('oasToDescriptions', () => {
-  const actual = oasToDescriptions(openApiSpec)
-  expect(actual).toMatchSnapshot()
-})
-
 test('fuzzyMatch', () => {
   const actual = fuzzyMatch('/artists/00FQb4jTyendYWaN8pK0wa/albums', '/artists/{artist_id}/albums')
   expect(actual).toBe(true)
 })
 
-test('treeShake', () => {
-  const endpoints = [
-    {
-      method: 'GET' as THttp,
-      baseUrl: '',
-      path: '/artists/00FQb4jTyendYWaN8pK0wa/albums',
-    },
-  ] as HttpRequestPlan[]
-  const actual = treeShake(openApiSpec, endpoints)
-  expect(actual).toMatchSnapshot()
-})
