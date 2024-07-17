c0ef86b1089f067ae2630bcdc050a55a00204525
mgarber93 <mgarber93@gmail.com>
2024-07-16 21:15:31 -0400

    Try tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -29,6 +29,9 @@ const router = createHashRouter([
 
 const App = () => (
   <Provider store={store}>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <RouterProvider router={router}/>
   </Provider>
 )
