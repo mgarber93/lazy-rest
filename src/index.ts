91b97fc247d87cbfb26e0d9c175421582ccf4870
mgarber93 <mgarber93@gmail.com>
2024-07-20 18:33:59 -0400

    Add uncaught exception handler idea
    Left todo

--- src/index.ts
+++ src/index.ts
@@ -72,4 +72,9 @@ app.on('activate', () => {
 })
 app.setName('Lazy Rest')
 
+process.on('uncaughtException', (err: Error) => {
+  const windowSender = container.resolve(WindowSender)
+  // windowSender.asyncSend('error', err) @todo implement error toast
+})
+
 setupInvokeHandlers()
