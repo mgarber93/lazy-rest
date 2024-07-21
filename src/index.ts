0a70246b7efd445ff34fc1632f4cc6c1af509594
mgarber93 <matthewgarber@proton.me>
2024-07-21 00:05:08 -0400

    Remove process on

--- src/index.ts
+++ src/index.ts
@@ -55,10 +55,6 @@ const createWindow = (): void => {
 app.on('ready', async () => {
   await installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
   createWindow()
-  process.on('uncaughtException', (err: Error) => {
-    const windowSender = container.resolve(WindowSender)
-    // windowSender.asyncSend('error', err) @todo implement error toast
-  })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
