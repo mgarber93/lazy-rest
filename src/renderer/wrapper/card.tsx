8d92b0642af4e1f64c3b83086e965e0ec1565139
mgarber93 <matthewgarber@proton.me>
2024-07-21 03:18:06 -0400

    Remove unused code
    To improve is to change; to perfect is to change often

--- src/renderer/wrapper/card.tsx
+++ src/renderer/wrapper/card.tsx
@@ -1,28 +1,10 @@
-import styled from 'styled-components'
 import {ReactNode} from 'react'
 
-const Div = styled.div`
-  border-radius: var(--border-radius-comfy);
-  border: 1px solid transparent;
-  background: var(--card);
-  padding: 0rem;
-  
-  &:hover {
-    border: 1px solid var(--background-color-4);
-  }
 
-  h2 {
-    font-size: medium;
-    color: var(--dark-grey)
-  }
-
-  margin: 0;
-  transition: 0.4s ease-in-out border;
-
-`
-
-export function Card(props: {children: ReactNode, slim?: boolean}) {
-  return <Div className={props?.slim ? 'slim' : ''}>
-    {props.children}
-  </Div>
+export function Card(props: { children: ReactNode }) {
+  return (
+    <div className="overflow-hidden rounded-lg bg-white dark:bg-black shadow">
+      <div className="px-4 py-5 sm:p-6">{props.children}</div>
+    </div>
+  )
 }
