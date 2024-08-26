f7b379771113b8e981fe89cebd99c3c43172a432
mgarber93 <matthewgarber@proton.me>
2024-08-26 18:06:13 -0400

    More header work

--- src/renderer/layouts/three-column-layout.tsx
+++ src/renderer/layouts/three-column-layout.tsx
@@ -1,32 +1,41 @@
 import React, {ReactElement, useCallback} from 'react'
 import {Cog6ToothIcon, PlusIcon} from '@heroicons/react/24/outline'
 
+export function HeaderTab({children, clickHandler}: { children: ReactElement, clickHandler: () => void }) {
+  return <>
+    <li className="h-full w-0 flex items-center">
+      <div className="h-full border-l border-zinc-400 dark:border-zinc-600"></div>
+    </li>
+    <li
+      onClick={clickHandler}
+      className="pl-[1.25rem] pr-[1.25rem] pt-2 pb-2 w-5 h-5 flex items-center justify-center h-full hover:bg-zinc-400 cursor-pointer"
+    >
+      <a
+        href="#"
+        className="group flex rounded-md text-sm font-semibold text-zinc-600"
+      >
+        {children}
+      </a>
+    </li>
+    <li className="h-full w-0 flex items-center">
+      <div className="h-full border-l border-zinc-400 dark:border-zinc-600"></div>
+    </li>
+  </>
+}
+
 export function Header() {
   const newPage = useCallback(() => {
     console.log('hello world')
   }, [])
-  return <header className="w-full h-10 bg-zinc-200 dark:bg-black opacity-dynamic drag top-0 z-60 flex flex-row">
-    <ul className="h-full ml-[6rem] flex items-center no-drag">
-      <li className="pt-2 pb-2 pr-4 w-5 h-5 flex items-center justify-center">
-        <a
-          href="#"
-          className="group flex rounded-md text-sm font-semibold leading-6 text-zinc-600"
-        >
-          <Cog6ToothIcon aria-hidden="true" className="h-[1.25rem] w-[1.25rem]"/>
-        </a>
-      </li>
-      <li className="h-full w-0 flex items-center">
-        <div className="h-full border-l border-gray-400 dark:border-gray-600"></div>
-      </li>
-      <li className="pl-4 pt-2 pb-2 pr-4 w-5 h-5 flex items-center justify-center">
-        <a
-          href="#"
-          className="group flex rounded-md text-sm font-semibold leading-6 text-zinc-600"
-          onClick={newPage}
-        >
-          <PlusIcon aria-hidden="true" className="h-[1.25rem] w-[1.25rem]"/>
-        </a>
-      </li>
+  return <header
+    className="w-full h-10 bg-zinc-200 dark:bg-black opacity-dynamic drag top-0 z-60 flex flex-row border-b border-zinc-400">
+    <ul className="h-full ml-[5rem] flex items-center no-drag">
+      <HeaderTab clickHandler={newPage}>
+        <Cog6ToothIcon aria-hidden="true" className="h-[1.25rem] w-[1.25rem]"/>
+      </HeaderTab>
+      <HeaderTab clickHandler={newPage}>
+        <PlusIcon aria-hidden="true" className="h-[1.25rem] w-[1.25rem]"/>
+      </HeaderTab>
     </ul>
   </header>
 }
