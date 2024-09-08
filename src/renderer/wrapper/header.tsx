8b500173cb70c1e26f61ba28c1e662c18f9fb558
mgarber93 <matthewgarber@proton.me>
2024-09-08 19:28:09 -0400

    Adjust header

--- src/renderer/wrapper/header.tsx
+++ src/renderer/wrapper/header.tsx
@@ -49,8 +49,8 @@ export function Header() {
     <header
       className={clsx(
         transparent,
-        "w-full sticky top-0 min-h-[37.5px] h-10 opacity-dynamic drag z-60 flex flex-row border-b-[0.5px] border-zinc-300 dark:border-b-zinc-950 shadow z-10",
-        "bg-white/90 dark:bg-zinc-950 pt-[1px] pb-[1px]",
+        "w-full sticky top-0 min-h-[37.5px] h-10 opacity-dynamic drag z-60 flex flex-row border-b-[0.5px] border-zinc-300 dark:border-b-zinc-950  z-10",
+        "bg-zinc-200/50 dark:bg-zinc-950 pt-[1px] pb-[1px]",
       )}>
       <ul className="w-full h-full ml-[5rem] flex items-center overflow-scroll gap-1">
         <HeaderTab to={"/config"} className={""}>
