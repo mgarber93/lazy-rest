184017cebcf6a7c25399be48092b6346c1a56f29
mgarber93 <mgarber93@gmail.com>
2024-09-07 22:16:13 -0400

    Add headless ui

--- src/renderer/pages/settings-page.tsx
+++ src/renderer/pages/settings-page.tsx
@@ -2,6 +2,7 @@ import {HeaderLayout} from '../layouts/header-layout'
 import {ApiForm} from '../wrapper/api-form'
 import React, {MutableRefObject, useRef} from 'react'
 import {ScrollPageLayout} from '../wrapper/scroll-container'
+import {OpenAiForm} from '../wrapper/open-ai-form'
 
 export function SettingsPage() {
   const sectionRefs = {
@@ -16,10 +17,15 @@ export function SettingsPage() {
         <ScrollPageLayout sectionRefs={sectionRefs}>
           <div ref={sectionRefs.ApiSpecifications} id={keys[0]} className={"min-h-[20rem]"}>
             <h2>Api Specifications</h2>
-            <ApiForm/>
+            <div className={"p-4"}>
+              <ApiForm/>
+            </div>
           </div>
           <div ref={sectionRefs.OpenAi} id={keys[1]} className={"min-h-[20rem]"}>
             <h2>Open AI</h2>
+            <div className={"p-4"}>
+              <OpenAiForm/>
+            </div>
           </div>
           <div ref={sectionRefs.BedRock} id={keys[2]} className={"min-h-[20rem]"}>
             <h2>AWS Bed Rock</h2>
