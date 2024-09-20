31e6e39e7d133e7782309c451a588f7949c92eb0
mgarber93 <matthewgarber@proton.me>
2024-09-20 16:51:32 -0400

    Styling changes

--- src/renderer/components/feed-content.tsx
+++ src/renderer/components/feed-content.tsx
@@ -4,10 +4,12 @@ import {CheckCircleIcon} from '@heroicons/react/24/outline'
 import {cardEffect} from '../utils/card'
 import {v4} from 'uuid'
 import {AuthoredContent} from '../../models/content'
+import {Button} from '@headlessui/react'
 
 
 export enum ActivityTypes {
   draft = 'draft',
+  done = 'done',
 }
 
 export interface ActivityItem {
@@ -24,14 +26,14 @@ const content = [
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: 'Search for artist Skrillex to get id'},
+    step: {name: 'Search for artist Skrillex to get id for Skrillex'},
     date: '2023-10-05',
     dateTime: '2023-10-05T18:30:00Z',
   },
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: `Search for artists with Skrillex's id within rest service`},
+    step: {name: `Query songs using Skrillex's id from step 1 sorted by play count`},
     date: '2023-10-04',
     dateTime: '2023-10-04T18:30:00Z',
   },
@@ -56,16 +58,19 @@ export function FeedContent() {
               <div className="w-px bg-gray-200 dark:bg-zinc-200"/>
             </div>
             <div className="relative flex h-6 w-6 flex-none items-center justify-center">
-              {activityItem.type === 'paid' ? (
+              {activityItem.type === ActivityTypes.done ? (
                 <CheckCircleIcon aria-hidden="true" className="h-6 w-6 text-green-600 bg-white rounded-xl"/>
               ) : (
                 <div className="h-3 w-3 rounded-full bg-gray-100 ring-1 ring-gray-300"/>
               )}
             </div>
             <p className="flex-auto">
-              <span
-                className={clsx("font-medium", activityItem.type === 'draft' && 'text-gray-500')}>{activityItem.step.name}</span>
+              <span className={clsx("font-medium", activityItem.type === 'draft' && 'text-gray-500')}>
+                {activityItem.step.name}
+              </span>
             </p>
+            <Button className={clsx("border hover:p-4 transition-all")}
+                    onClick={() => console.log('hello world')}>Test</Button>
             <time dateTime={activityItem.dateTime} className="flex-none">
               {activityItem.date}
             </time>
