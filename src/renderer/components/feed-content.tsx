b6206e60357d4d45a5ee57018a6578890ed93dbd
mgarber93 <matthewgarber@proton.me>
2024-09-27 22:51:16 -0400

    Add HTTP call card for active feed content
    Introduced a new `HttpCallCard` component to handle HTTP request simulations. Updated the `FeedContent` component to display the `HttpCallCard` only for active items.

--- src/renderer/components/feed-content.tsx
+++ src/renderer/components/feed-content.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import {cardEffect} from '../utils/card'
 import {v4} from 'uuid'
 import {AuthoredContent} from '../../models/content'
-import {Button} from '@headlessui/react'
+import {Button, Input, Select} from '@headlessui/react'
 
 
 export enum ActivityTypes {
+  active = 'active',
   draft = 'draft',
   done = 'done',
 }
@@ -24,7 +25,7 @@ export interface ActivityItem {
 const content = [
   {
     id: v4(),
-    type: ActivityTypes.draft,
+    type: ActivityTypes.active,
     step: {name: 'Search for artist Skrillex to get id for Skrillex'},
     date: '2023-10-05',
     dateTime: '2023-10-05T18:30:00Z',
@@ -42,23 +43,47 @@ export interface FeedContentProps {
   content?: AuthoredContent
 }
 
+export function HttpCallCard() {
+  const elements = `border rounded-xl bg-black/15 border-zinc-700`
+  return <div className={"p-4 flex flex-row gap-2"}>
+    <Select name="status"
+            className={clsx(elements, "h-full bg-transparent data-[hover]:shadow data-[focus]:bg-blue-100")}
+            aria-label="Project status">
+      <option value="get">Get</option>
+      <option value="post">Post</option>
+      <option value="put">Put</option>
+      <option value="delete">Delete</option>
+    </Select>
+    <Input
+      className={clsx(
+        elements,
+        'flex-grow py-1.5 px-3 text-sm/6 text-white',
+        'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
+      )}
+    />
+    <Button className={clsx(elements, "px-2 border-zinc-500")}
+            onClick={() => console.log('hello world')}>Send</Button>
+  </div>
+}
+
 export function FeedContent() {
   return (
-    <div className={clsx(cardEffect, " p-4 rounded-xl")}>
+    <div className={clsx(cardEffect, "p-4 rounded-xl")}>
       <div className={'text-sm mb-2 w-full border-b border-gray-200 dark:border-zinc-600 text-gray-500'}>
         Call plan
       </div>
-      <ul role="list" className="space-y-0">
+      <ul role="list" className="space-y-2">
         {content.map((activityItem, activityItemIdx) => (
           <li key={activityItem.id}
-              className={clsx("relative flex gap-x-4", activityItemIdx % 2 === 0 ? 'bg-zinc-900' : 'bg-zinc-800', 'border-b border-gray-600')}>
+              className={clsx("relative flex flex-col gap-x-4", 'bg-black/15 border p-4 rounded-2xl border-gray-600')}>
             <p className="flex-auto">
               <span className={clsx("font-medium", activityItem.type === 'draft' && 'text-gray-500')}>
                 {activityItem.step.name}
               </span>
             </p>
-            <Button className={clsx("border hover:p-4 transition-all")}
-                    onClick={() => console.log('hello world')}>Detail</Button>
+            {
+              activityItem.type === 'active' && <HttpCallCard/>
+            }
           </li>
         ))}
       </ul>
