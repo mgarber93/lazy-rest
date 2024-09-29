a98c9c16a8cace663046a6fb82d0fcb600de2907
mgarber93 <matthewgarber@proton.me>
2024-09-29 10:03:26 -0400

    Connect step info

--- src/renderer/components/feed-content.tsx
+++ src/renderer/components/feed-content.tsx
@@ -1,11 +1,10 @@
 import clsx from 'clsx'
 import React, {ReactNode} from 'react'
 import {v4} from 'uuid'
-import {AppButton} from './app-button'
 import {PlusIcon} from '@heroicons/react/24/outline'
+import {Input, Select} from '@headlessui/react'
+import {AppButton} from './app-button'
 import {Card, CardH3, CardSection} from '../wrapper/card'
-import {Button, Input, Select} from '@headlessui/react'
-
 
 export enum ActivityTypes {
   active = 'active',
@@ -17,45 +16,106 @@ export enum ActivityTypes {
 export interface ActivityItem {
   id: string;
   type: string;
-  step: {
+  step: Partial<{
     name: string;
-  };
+    httpVerb: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS';
+    url: string;
+    queryParams: object;
+    headers: object;
+  }>;
 }
 
 const content = [
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: 'Search for Artist “Skrillex”: Retrieve Skrillex’s artist ID by searching for his name using the Spotify API.'},
+    step: {
+      name: 'Search for Artist “Skrillex”: Retrieve Skrillex’s artist ID by searching for his name using the Spotify API.',
+      httpVerb: 'GET',
+      url: 'https://api.spotify.com/v1/search',
+      queryParams: {
+        q: 'Skrillex',
+        type: 'artist',
+        limit: '1',
+      },
+      headers: {
+        Authorization: 'Bearer {access_token}',
+        'Content-Type': 'application/json',
+      },
+    },
   },
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: `Get Skrillex’s Top Tracks: Use the artist ID to fetch his top tracks from Spotify`},
+    step: {
+      name: `Get Skrillex’s Top Tracks: Use the artist ID to fetch his top tracks from Spotify`,
+      httpVerb: 'GET',
+      url: 'https://api.spotify.com/v1/artists/{artist_id}/top-tracks',
+      queryParams: {
+        country: 'US',
+      },
+      headers: {
+        Authorization: 'Bearer {access_token}',
+        'Content-Type': 'application/json',
+      },
+    },
   },
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: `Get Your Spotify User ID: Obtain your user ID by accessing your Spotify profile information.`},
+    step: {
+      name: `Get Your Spotify User ID: Obtain your user ID by accessing your Spotify profile information.`,
+      httpVerb: 'GET',
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        Authorization: 'Bearer {access_token}',
+        'Content-Type': 'application/json',
+      },
+    },
   },
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: `Create a New Playlist: Create a new playlist in your account to hold Skrillex’s top tracks.`},
+    step: {
+      name: `Create a New Playlist: Create a new playlist in your account to hold Skrillex’s top tracks.`,
+      httpVerb: 'POST',
+      url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
+      headers: {
+        Authorization: 'Bearer {access_token}',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        name: 'Skrillex Top Tracks',
+        description: 'A playlist of top Skrillex tracks',
+        public: true,
+      },
+    },
   },
   {
     id: v4(),
     type: ActivityTypes.draft,
-    step: {name: `Add Tracks to the Playlist: Add the retrieved top tracks to your new playlist.`},
+    step: {
+      name: `Add Tracks to the Playlist: Add the retrieved top tracks to your new playlist.`,
+      httpVerb: 'POST',
+      url: 'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
+      headers: {
+        Authorization: 'Bearer {access_token}',
+        'Content-Type': 'application/json',
+      },
+      body: {
+        uris: ['spotify:track:1', 'spotify:track:2', '...'], // Replace with actual track URIs
+      },
+    },
   },
 ] as ActivityItem[]
 
-export function HttpCallForm() {
-  const elements = `border rounded-xl bg-black/5 border-zinc-700`
+export function HttpCallForm({step}: { step?: ActivityItem['step'] }) {
+  const elements = `border rounded-xl bg-transparent border-zinc-700`
   return <div className={"p-4 flex flex-row gap-2"}>
     <Select name="status"
             className={clsx(elements, "h-full bg-transparent data-[hover]:shadow data-[focus]:bg-blue-100")}
-            aria-label="Project status">
+            aria-label="Project status"
+            defaultValue={step?.httpVerb?.toLowerCase()}>
       <option value="get">Get</option>
       <option value="post">Post</option>
       <option value="put">Put</option>
@@ -64,12 +124,13 @@ export function HttpCallForm() {
     <Input
       className={clsx(
         elements,
-        'flex-grow py-1.5 px-3 text-sm/6 text-white',
+        'flex-grow py-1.5 px-3 text-sm/6 dark:text-white',
         'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
       )}
+      defaultValue={step?.url}
     />
-    <Button className={clsx(elements, "px-2 border-zinc-500")}
-            onClick={() => console.log('hello world')}>Send</Button>
+    <AppButton className={clsx(elements, "px-2 border-zinc-500")}
+               onClick={() => console.log(step)}>Send</AppButton>
   </div>
 }
 
@@ -78,7 +139,7 @@ export function HttpCallCard({activity, index}: { activity: any, index?: number 
     <div className={"h-full rounded-xl p-2"}>
       <CardH3>{(index ?? 0) + 1}) {activity.step.name}</CardH3>
     </div>
-    <HttpCallForm/>
+    <HttpCallForm step={activity.step}/>
   </CardSection>
 }
 
@@ -90,12 +151,6 @@ export function PlanableComponent() {
   </div>
 }
 
-export function DraftActivity() {
-
-}
-
-export function PlanableActivity() {
-}
 
 export function ActiveActivity({activity}: { activity: ActivityItem }) {
   return <li key={activity.id}
