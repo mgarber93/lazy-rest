0240e909c15e99301520ff9f4a3a264a7138772f
mgarber93 <matthewgarber@proton.me>
2024-09-29 11:36:12 -0400

    Add ollama section

--- src/renderer/pages/settings-page.tsx
+++ src/renderer/pages/settings-page.tsx
@@ -11,9 +11,10 @@ export function SettingsPage() {
   const dispatch = useAppDispatch()
   const apis = useAppSelector(state => state.tools.api)
   const sectionRefs = {
-    ApiSpecifications: useRef<HTMLDivElement>(null),
+    Ollama: useRef<HTMLDivElement>(null),
     OpenAi: useRef<HTMLDivElement>(null),
     BedRock: useRef<HTMLDivElement>(null),
+    ApiSpecifications: useRef<HTMLDivElement>(null),
   } as Record<string, MutableRefObject<HTMLDivElement>>
   const keys = Object.keys(sectionRefs)
   useEffect(() => {
@@ -24,6 +25,30 @@ export function SettingsPage() {
     <HeaderLayout>
       <div className="w-full h-full">
         <ScrollPageLayout sectionRefs={sectionRefs}>
+          <Card>
+            <div ref={sectionRefs.Ollama} id={keys[1]} className={"min-h-[20rem]"}>
+              <CardH2>Ollama</CardH2>
+              <div className="py-4">
+              
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div ref={sectionRefs.OpenAi} id={keys[1]} className={"min-h-[20rem]"}>
+              <CardH2>Open AI</CardH2>
+              <div className={"py-4"}>
+                <OpenAiForm/>
+              </div>
+            </div>
+          </Card>
+          
+          <Card>
+            <div ref={sectionRefs.BedRock} id={keys[2]} className={"min-h-[20rem]"}>
+              <CardH2>AWS Bed Rock</CardH2>
+              <div>todo</div>
+            </div>
+          </Card>
+          
           <Card className={"pb-4"}>
             <div ref={sectionRefs.ApiSpecifications} id={keys[0]} className={""}>
               <CardH2>Api Specifications</CardH2>
@@ -40,22 +65,6 @@ export function SettingsPage() {
               <CardH3>Add new</CardH3>
               <ApiForm/>
             </CardSection>
-          </Card>
-          
-          <Card>
-            <div ref={sectionRefs.OpenAi} id={keys[1]} className={"min-h-[20rem]"}>
-              <CardH2>Open AI Configuration</CardH2>
-              <div className={"py-4"}>
-                <OpenAiForm/>
-              </div>
-            </div>
-          </Card>
-          
-          <Card>
-            <div ref={sectionRefs.BedRock} id={keys[2]} className={"min-h-[20rem]"}>
-              <CardH2>AWS Bed Rock</CardH2>
-              <div>todo</div>
-            </div>
           </Card>
         </ScrollPageLayout>
       </div>
