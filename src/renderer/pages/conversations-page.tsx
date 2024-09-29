90971dec2ac56a8354a068e46c4d107edd3970eb
mgarber93 <matthewgarber@proton.me>
2024-09-29 15:07:56 -0400

    Refactor

--- src/renderer/pages/conversations-page.tsx
+++ src/renderer/pages/conversations-page.tsx
@@ -1,19 +1,15 @@
-import React, {ChangeEvent, KeyboardEventHandler, MutableRefObject, useCallback, useState} from 'react'
+import React, {MutableRefObject, useState} from 'react'
 import clsx from 'clsx'
-import {Field, Input, Select} from '@headlessui/react'
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 import {HeaderLayout} from '../layouts/header-layout'
 import {useCurrentConversation} from '../hooks/current-conversation'
 import {ScrollPageLayout} from '../layouts/scroll-container'
-import {Card, cardEffect} from '../wrapper/card'
-import {useAppDispatch, useAppSelector} from '../features/store'
-import {appendContent, setResponder, streamResponse} from '../features/chat'
-import {AuthoredContent, createContent} from '../../models/content'
-import {User} from '../../models/user'
-import {Responder, TModel} from '../../models/responder'
+import {Card} from '../wrapper/card'
+import {AuthoredContent} from '../../models/content'
 import {FeedContent} from '../components/feed-content'
+import {UserInputForm} from './user-input-form'
 
 export function ConversationContent({content}: { content: AuthoredContent }) {
   if (content.apiCallPlan) {
@@ -36,54 +32,8 @@ export function ConversationContent({content}: { content: AuthoredContent }) {
 }
 
 export function ConversationsPage() {
-  const dispatch = useAppDispatch()
   const conversation = useCurrentConversation()
-  const user = useAppSelector((state) => state.user) as User
-  
-  const lazyRest = "REST"
-  
   const [sectionRefs] = useState<Record<string, MutableRefObject<HTMLDivElement | null>>>({})
-  const [value, setValue] = useState('')
-  const handleKeyPress: KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const {value} = e.target as { value: string }
-    if (e.key === 'Enter' && !e.shiftKey && value && conversation.responder) {
-      e.preventDefault()
-      const prompt = createContent(value, conversation.id, user?.username, 'user')
-      dispatch(appendContent(prompt))
-      dispatch(streamResponse({conversationId: conversation.id}))
-      setValue('')
-    }
-  }, [conversation, user])
-  const handleOnChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }, [setValue])
-  const models = useAppSelector((state) => state.models.models)
-  const tools = useAppSelector((state) => state.tools)
-  const handleModelChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    const model = e.target.value as TModel | string
-    if (model === lazyRest) {
-      dispatch(setResponder({
-        responder: {
-          type: 'organization',
-          provider: 'openai',
-          model: model as TModel,
-        } satisfies Responder,
-        chatId: conversation.id,
-      }))
-    } else {
-      dispatch(setResponder({
-        responder: {
-          type: 'chat',
-          provider: 'openai',
-          model: model as TModel,
-        } satisfies Responder,
-        chatId: conversation.id,
-      }))
-    }
-  }, [conversation])
-  
   return (
     <HeaderLayout>
       <div className={clsx("w-full h-full")}>
@@ -96,32 +46,7 @@ export function ConversationsPage() {
               conversation.content.map((content, index) => <ConversationContent content={content} key={index}/>)
             }
             <div className={clsx("mt-auto")}>
-              <Field className={"flex w-full flex-row-reverse gap-x-2 bottom-2 ml-auto pt-4"}>
-                <Input
-                  className={clsx(
-                    cardEffect,
-                    'leading-relaxed text-xl flex bg-zinc-50/90 shadow-2xl dark:shadow-zinc-800 dark:hover:shadow-zinc-600 border-0 w-full mt-auto',
-                  )}
-                  onKeyUpCapture={handleKeyPress}
-                  value={value}
-                  onChange={handleOnChange}
-                >
-                </Input>
-                <Select
-                  name={"responder"}
-                  aria-label={"responder"}
-                  className={clsx(cardEffect, "leading-relaxed text flex bg-zinc-50/90 shadow-2xl dark:shadow-none z-1 border-0")}
-                  value={conversation?.responder?.model}
-                  onChange={handleModelChange}
-                >
-                  {
-                    models.map((model) => <option value={model} key={model}>{model}</option>)
-                  }
-                  {
-                    Object.keys(tools.api).length > 0 && <option value={lazyRest}>Lazy Rest</option>
-                  }
-                </Select>
-              </Field>
+              <UserInputForm/>
             </div>
           </div>
         </ScrollPageLayout>
