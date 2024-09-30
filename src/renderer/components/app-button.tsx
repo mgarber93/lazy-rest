efdda227f0cd588d4b303d014d1d0613a7e01bd8
mgarber93 <matthewgarber@proton.me>
2024-09-30 19:19:03 -0400

    Use motion button

--- src/renderer/components/app-button.tsx
+++ src/renderer/components/app-button.tsx
@@ -1,6 +1,7 @@
-import {Button} from '@headlessui/react'
 import React, {ReactNode} from 'react'
 import clsx from 'clsx'
+
+import {motion} from 'framer-motion'
 
 export function AppButton({children, onClick, disabled, className}: {
   children: ReactNode,
@@ -8,15 +9,21 @@ export function AppButton({children, onClick, disabled, className}: {
   disabled?: boolean,
   className?: string
 }) {
-  const button = "border border-black/50 dark:border-neutral-700 dark:bg-neutral-950 bg-transparent rounded py-2 px-4 w-fit max-w-36 rounded-lg"
-  const hoverFlip = "hover:dark:bg-white hover:bg-black hover:text-white hover:dark:border-black hover:dark:text-black"
+  const button = "border border-black/50 dark:border-neutral-700 dark:bg-transparent bg-transparent rounded py-2 px-4 max-w-36 rounded-lg"
+  const hoverFlip = "hover:dark:bg-white/80 hover:bg-black hover:text-white hover:dark:border-black hover:dark:text-black"
   return (
-    <Button className={clsx(
-      "bg-white/5 transition-all p-1 rounded w-full h-full flex items-center justify-center",
-      button,
-      hoverFlip,
-      className,
-    )} onClick={onClick} disabled={disabled}>
+    <motion.button
+      whileTap={{scale: 0.85}}
+      transition={{duration: 0.001}}
+      className={clsx(
+        button,
+        hoverFlip,
+        "bg-white/5 transition-all p-1 rounded h-full flex items-center justify-center",
+        className,
+      )}
+      onClick={onClick} disabled={disabled}
+    >
       {children}
-    </Button>)
+    </motion.button>
+  )
 }
