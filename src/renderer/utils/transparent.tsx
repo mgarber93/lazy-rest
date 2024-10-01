d335c14098ee89b264dd44c0d108fe4e4bd7e309
mgarber93 <mgarber93@gmail.com>
2024-10-01 02:51:29 -0400

    Adjust transparency

--- src/renderer/utils/transparent.tsx
+++ src/renderer/utils/transparent.tsx
@@ -1,2 +1,2 @@
 export const transparent = "bg-neutral-200/[98%] dark:bg-neutral-800/[90%] backdrop-blur-sm"
-export const lgTransparent = "bg-neutral-100/[83%] dark:bg-neutral-800/[95%] dark:from-neutral-900 dark:to-neutral-950"
+export const lgTransparent = "bg-neutral-100/[83%] dark:bg-neutral-800/[85%] dark:from-neutral-900 dark:to-neutral-950"
