0e94883b0a2c7e153e6580155cc5109b958e3915
mgarber93 <mgarber93@gmail.com>
2024-10-01 01:41:07 -0400

    Adjust amber

--- src/renderer/pages/conversations-page.tsx
+++ src/renderer/pages/conversations-page.tsx
@@ -23,7 +23,7 @@ export function ConversationContent({content}: { content: AuthoredContent }) {
       )}
     >
       {
-        content.role === "user" && <CardSection className={"dark:bg-amber-100/[5%]"}>
+        content.role === "user" && <CardSection className={"dark:bg-amber-800/5"}>
               <span
                 className={clsx('flex-1')}
                 key={content.id}
