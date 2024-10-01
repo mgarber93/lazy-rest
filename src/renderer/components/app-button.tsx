6f719f7fb4cf076790f622ab38cb050cc53b8874
mgarber93 <matthewgarber@proton.me>
2024-09-30 23:00:36 -0400

    Implement button

--- src/renderer/components/app-button.tsx
+++ src/renderer/components/app-button.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react'
+import React, {ReactNode, useState} from 'react'
 import clsx from 'clsx'
 
 import {motion} from 'framer-motion'
@@ -11,17 +11,27 @@ export function AppButton({children, onClick, disabled, className}: {
 }) {
   const button = "border border-black/50 dark:border-neutral-700 dark:bg-transparent bg-transparent rounded py-2 px-4 max-w-36 rounded"
   const hoverFlip = "hover:dark:bg-white/80 hover:bg-black hover:text-white hover:dark:border-black hover:dark:text-black"
+  const [isMouseUp, setIsMouseUp] = useState(false)
+  const handleMouseUp = () => {
+    setIsMouseUp(true)
+    setTimeout(() => {
+      setIsMouseUp(false)
+    }, 300) // resets back to 1 after 300ms
+  }
   return (
     <motion.button
-      whileTap={{scale: 0.95}}
-      transition={{duration: 0.001}}
+      whileTap={{scale: 0.96}}
+      animate={isMouseUp ? {scale: [1, 1.01, 1.01, 1]} : undefined}
+      transition={isMouseUp ? {duration: 0.3} : undefined} // Slower on mouse up
       className={clsx(
         button,
         hoverFlip,
-        "bg-white/5 transition-all p-1 rounded h-full flex items-center justify-center",
+        "bg-white/5 p-1 rounded h-full flex items-center justify-center",
         className,
       )}
-      onClick={onClick} disabled={disabled}
+      onClick={onClick}
+      disabled={disabled}
+      onMouseUp={handleMouseUp}
     >
       {children}
     </motion.button>
