ccf3ad08bf834d10ab47a6c6da964f5f2e580a69
mgarber93 <matthewgarber@proton.me>
2024-10-11 01:04:25 -0400

    Flat

--- src/renderer/layouts/scroll-container.tsx
+++ src/renderer/layouts/scroll-container.tsx
@@ -69,13 +69,12 @@ export function ScrollPageLayout({sections, children}: {
   children?: ReactNode,
 }) {
   const effect = "border border-transparent border-black/5 h-full"
-  const background = "bg-amber-50/5 dark:bg-neutral-900/[75%] shadow-xl"
+  const background = "bg-amber-50/5 dark:bg-black shadow-xl"
   return <div className={clsx('h-[calc(100vh-46.993px)]')}>
     <Center>
       <div className={clsx("lg:col-span-1 col-span-2 h-[calc(100vh-47px)]")}>
         <aside className={clsx(effect,
           "rounded-l rounded-bl rounded-br-3xl rounded-tr-3xl",
-          background,
         )}>
           <Sections sections={sections}/>
         </aside>
@@ -83,7 +82,6 @@ export function ScrollPageLayout({sections, children}: {
       <div className={clsx(
         "col-span-4 top-4 rounded",
         "h-[calc(100vh-47px)] mb-2 overflow-y-scroll",
-        background,
         effect,
       )}>
         <div className={"p-4"}>
@@ -101,7 +99,6 @@ export function ScrollUserInputPageLayout({sections, children}: {
   children?: ReactNode,
 }) {
   const effect = "border border-transparent border-black/5 h-full"
-  const background = "bg-neutral-50/50 bg-neutral-50 dark:bg-neutral-900/[95%] shadow-xl"
   const convo = useCurrentConversation()
   const contentRef = useRef<HTMLDivElement | null>(null)
   const [isScrollable, setIsScrollable] = useState(false)
@@ -134,7 +131,6 @@ export function ScrollUserInputPageLayout({sections, children}: {
           transition={{duration: 0.03}}
           className={clsx(effect,
             "rounded-l rounded-bl rounded-br-3xl rounded-tr-3xl",
-            background,
             "row-span-2 col-span-1 lg:col-span-1",
           )}
         >
@@ -158,7 +154,6 @@ export function ScrollUserInputPageLayout({sections, children}: {
           className={clsx(
             "col-span-4 top-4 rounded",
             "h-[calc(100vh-47px)] mb-2 overflow-y-scroll",
-            background,
             effect,
             "row-span-1 col-span-5 lg:col-span-4",
             "p-0 bg-transparent",
