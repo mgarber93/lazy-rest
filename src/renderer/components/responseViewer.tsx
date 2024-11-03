8feb6970ae31853ec5636ccff0ac961ef1d65d83
mgarber93 <matthewgarber@proton.me>
2024-11-03 02:56:47 -0500

    Remove unneeded monaco config
    Probably not needed until loaded locally

--- src/renderer/components/responseViewer.tsx
+++ src/renderer/components/responseViewer.tsx
@@ -2,18 +2,6 @@ import React from 'react'
 import {Editor} from '@monaco-editor/react'
 import clsx from 'clsx'
 
-// In your React component or renderer entry point
-(window as any).MonacoEnvironment = {
-  getWorkerUrl: function (_moduleId: string, label: string) {
-    if (label === 'json') return './worker/json.worker.js'
-    if (label === 'css') return './worker/css.worker.js'
-    if (label === 'html') return './worker/html.worker.js'
-    if (label === 'typescript' || label === 'javascript')
-      return './worker/ts.worker.js'
-    return './worker/editor.worker.js'
-  },
-}
-
 export function ResponseViewer({response}: { response: { data: object } | null }) {
   const code = response ? JSON.stringify(response.data, null, 2) : '// Send to get response'
   
