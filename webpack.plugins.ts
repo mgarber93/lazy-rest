694839be8b5f57c0851ba652a8fdc0b528ac9d90
mgarber93 <matthewgarber@proton.me>
2025-01-29 18:46:07 -0500

    Add CopyPlugin to handle HTML file copying
    Introduce CopyPlugin in the renderer config to copy `index.html` into the output directory. Removed redundant CopyPlugin import from `webpack.plugins.ts` to keep the configuration clean. Updated file extensions in `resolve` to include `.scss`.

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -2,8 +2,6 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const CopyPlugin = require('copy-webpack-plugin')
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,7 +3,8 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 
 import {rules} from './webpack.rules'
 import {plugins} from './webpack.plugins'
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyPlugin = require('copy-webpack-plugin')
 
 export const rendererConfig: Configuration = {
   module: {
@@ -11,15 +12,21 @@ export const rendererConfig: Configuration = {
   },
   plugins: [
     ...plugins,
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/renderer/index.html', to: '.' }
+      ],
+    }),
     new MonacoWebpackPlugin({
       languages: ['javascript', 'typescript', 'css', 'html', 'json'],
       filename: 'worker/[name].worker.js',
     }),
   ],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss'],
   },
   output: {
     publicPath: '',
   },
 }
+
