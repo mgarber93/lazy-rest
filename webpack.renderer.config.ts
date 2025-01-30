fe088994f6e16da160f623f9d7d9201dd918a7b4
mgarber93 <matthewgarber@proton.me>
2025-01-29 22:47:59 -0500

    Add CopyPlugin to handle HTML file copying
    Introduce CopyPlugin in the renderer config to copy `index.html` into the output directory. Removed redundant CopyPlugin import from `webpack.plugins.ts` to keep the configuration clean. Updated file extensions in `resolve` to include `.scss`.

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,8 +3,7 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 
 import {rules} from './webpack.rules'
 import {plugins} from './webpack.plugins'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const CopyPlugin = require('copy-webpack-plugin')
+
 
 export const rendererConfig: Configuration = {
   module: {
@@ -12,21 +11,15 @@ export const rendererConfig: Configuration = {
   },
   plugins: [
     ...plugins,
-    new CopyPlugin({
-      patterns: [
-        { from: 'src/renderer/index.html', to: '.' }
-      ],
-    }),
     new MonacoWebpackPlugin({
       languages: ['javascript', 'typescript', 'css', 'html', 'json'],
       filename: 'worker/[name].worker.js',
     }),
   ],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss'],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
   output: {
     publicPath: '',
   },
 }
-
