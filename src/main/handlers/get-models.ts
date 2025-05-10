1f1d5a15d6091f774ac6f543f52125ab729d723a
mgarber93 <matthewgarber@proton.me>
2025-05-10 12:30:49 -0400

    Get bedrock models

--- src/main/handlers/get-models.ts
+++ src/main/handlers/get-models.ts
@@ -3,11 +3,13 @@ import {Handler} from './handler'
 import {container} from 'tsyringe'
 import {OpenAiProvider} from '../providers/openai'
 import {OllamaProvider} from '../providers/ollama'
+import {BedrockProvider} from '../providers/bedrock'
 
 export class ModelListHandle implements Handler<'getModels'> {
   private openAiLlm: OpenAiProvider = container.resolve(OpenAiProvider)
   private ollama = container.resolve(OllamaProvider)
-  
+  private bedrock = container.resolve(BedrockProvider)
+
   async handle(provider: TProvider) {
     console.log('GetModelsHandler:handle', provider)
     switch (provider) {
@@ -16,6 +18,9 @@ export class ModelListHandle implements Handler<'getModels'> {
       }
       case "ollama": {
         return this.ollama.list()
+      }
+      case "bedrock": {
+        return this.bedrock.list()
       }
       default: {
         throw new Error('not implemented')
