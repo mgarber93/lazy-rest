65b59a827357d2c34a0e785293b7ee90f3a48fbd
mgarber93 <mgarber93@gmail.com>
2025-05-11 16:20:12 -0400

    Support light mode

--- src/renderer/components/app-combobox.tsx
+++ src/renderer/components/app-combobox.tsx
@@ -16,22 +16,22 @@ export interface PropsAppComboBox {
 
 export function AppCombobox({options, onChange, initialSelected}: PropsAppComboBox) {
   const [query, setQuery] = useState('')
-  const [selected, setSelected] = useState(initialSelected || options[0] || { id: 0, name: '' })
-
+  const [selected, setSelected] = useState(initialSelected || options[0] || {id: 0, name: ''})
+  
   const filteredOptions =
     query === ''
       ? options
       : options.filter((o) => {
         return o.name.toLowerCase().includes(query.toLowerCase())
       })
-
+  
   const handleChange = (value: ComboSelectable) => {
     setSelected(value)
     if (onChange) {
       onChange(value)
     }
   }
-
+  
   return (
     <Combobox value={selected} onChange={handleChange}
       onClose={() => setQuery('')}
@@ -39,14 +39,15 @@ export function AppCombobox({options, onChange, initialSelected}: PropsAppComboB
       <div className="relative">
         <ComboboxInput
           className={clsx(
-            'w-full rounded border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-white',
+            'w-full rounded border-none bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-black dark:text-white',
             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
           )}
           displayValue={(selectable: ComboSelectable) => selectable?.name ?? ''}
           onChange={(event) => setQuery(event.target.value)}
         />
         <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-          <ChevronDownIcon className="size-4 fill-white/60 group-data-[hover]:fill-white"/>
+          <ChevronDownIcon
+            className="size-4 fill-black/60 group-data-[hover]:fill-black dark:fill-white/60 dark:group-data-[hover]:fill-white"/>
         </ComboboxButton>
       </div>
       
@@ -54,7 +55,7 @@ export function AppCombobox({options, onChange, initialSelected}: PropsAppComboB
         anchor="bottom"
         transition
         className={clsx(
-          'w-[var(--input-width)] max-h-[400px] overflow-y-auto rounded border border-white/5 bg-neutral-950 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
+          'w-[var(--input-width)] max-h-[400px] overflow-y-auto rounded border border-black/5 dark:border-white/5 bg-white dark:bg-neutral-950 p-1 [--anchor-gap:var(--spacing-1)] empty:invisible',
           'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0 ',
         )}
       >
@@ -62,10 +63,10 @@ export function AppCombobox({options, onChange, initialSelected}: PropsAppComboB
           <ComboboxOption
             key={selectable.id}
             value={selectable}
-            className="group flex cursor-default items-center gap-2 rounded py-1.5 px-3 select-none data-[focus]:bg-white/10"
+            className="group flex cursor-default items-center gap-2 rounded py-1.5 px-3 select-none data-[focus]:bg-black/10 dark:data-[focus]:bg-white/10"
           >
-            <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible"/>
-            <div className="text-sm/6 text-white">{selectable.name}</div>
+            <CheckIcon className="invisible size-4 fill-black dark:fill-white group-data-[selected]:visible"/>
+            <div className="text-sm/6 text-black dark:text-white">{selectable.name}</div>
           </ComboboxOption>
         ))}
       </ComboboxOptions>
